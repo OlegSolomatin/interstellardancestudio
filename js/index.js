fe2854ae90cfc2559c946d6d90af52f3542619f4
OlegSolomatin <olegsolomatin2018@yandex.ru>
2020-08-19 15:54:17 +0300

    update first slide 1. fix bugs close add content teacher 1. fix content position 2. fix bug close z-index 3. update adaptive

--- js/index.js
+++ js/index.js
@@ -54,48 +54,21 @@ $(document).ready(function() {
         $closeBtn.addClass('show-close');
         $iconTwitter.addClass('icon-show');
 
-        var animFalse = function() {
-            animation = false;
-        };
 
-        var active = function() {
-            if (animation) return;
-            var cellData = $(this).data('cell');
-            var $content = $('.box__content[data-content=' + cellData + ']');
-            animation = true;
 
-            $cell.addClass('cell-fade');
-            $(this).addClass('active');
-            $content.addClass('show-content');
-            $closeBtn2.addClass('box-close-active');
-
-            _this.find('.box-close-active').css({
-                'display': 'block',
-                'transition': '2s'
-            });
-
-            $closeBtn.removeClass('show-close');
-            $iconTwitter.removeClass('icon-show');
-
-            setTimeout(animFalse, animDur);
-        };
-
-        var close = function() {
-            animation = true;
-
-            $cell.removeClass('active cell-fade');
-            $content.removeClass('show-content');
-            $(this).removeClass('box-close-active');
-
-            _this.find('.box__close').css({
-                'display': 'none'
-            });
-
-            $closeBtn.addClass('show-close');
-            $iconTwitter.addClass('icon-show');
-
-            setTimeout(animFalse, animDur);
-        };
+        /*Проверка CSS свойства блока team details*/
+        /*var activepersone = function () {
+            if  ($('.team-detail').css('visibiliti') !== 'visible'){
+                _this.find('.box-close-active').css({
+                    'display': 'block',
+                    'transition': '2s'
+                });
+            } else {
+                _this.find('.box__close').css({
+                    'display': 'none'
+                });
+            }
+        };*/
 
         $row.on('click', '.box__row-cell', active);
         $closeBtn2.on('click', close);
@@ -144,35 +117,23 @@ $(document).ready(function() {
                     'display': 'block',
                     'font-size': '35px'
                 });
+                _this.find('.slide__text--1').css({
+                    'left': '8%'
+                });
             } else {
                 _this.find('.slide__text').css({
-                    /*'transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',
-                    '-moz-transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',*/
                     'opacity': '1',
                     'transition': '750ms',
                     '-webkit-transition': '750ms',
                     'display': 'block',
                     'font-size': '35px',
-                    'transform': 'ratate(90deg)',
+                    'transform': 'rotate(90deg)',
                     'left': '-14%',
                     'width': '50%',
                 });
             }
         }
-        $(window).load(windowSize);
-        /*_this.find('.slide__text').css({
-            'transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',
-            '-moz-transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',
-            'opacity': '1',
-            'transition': '750ms',
-            '-webkit-transition': '750ms',
-            'display': 'block',
-            'font-size': '35px'
-        });*/
-
-        _this.find('.slide__text--1').css({
-            'left': '8%'
-        });
+        $(window).on('load resize',windowSize);
 
         for(let i = target, length = $slide.length; i < length; i++) {
             $('.slide--' + (i + 1)).css({
@@ -218,10 +179,6 @@ $(document).ready(function() {
             'opacity': '0'
         });
 
-        /*_this.find('.slide__text--1').css({
-            'left': '4%'
-        });*/
-
         function windowSize(){
             if ($(window).width() > '576'){
                 _this.find('.slide__text').css({
@@ -237,28 +194,18 @@ $(document).ready(function() {
                 });
             } else {
                 _this.find('.slide__text').css({
-                    /*'transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',
-                    '-moz-transform': 'translate3d(0, -40%, 0) rotate(0.01deg)',*/
                     'opacity': '1',
                     'transition': '750ms',
                     '-webkit-transition': '750ms',
                     'display': 'block',
                     'font-size': '30px',
-                    'transform': 'ratate(90deg)',
+                    'transform': 'rotate(90deg)',
                     'left': '-14%',
                     'width': '50%',
                 });
             }
         }
-        $(window).load(windowSize);
-        /*$text.css({
-            'transform': 'translate3d(0, -50%, 0) rotate(0.01deg)',
-            'opacity': '1',
-            'transition': '200ms',
-            '-webkit-transition': '200ms',
-            'display': 'block',
-            'font-size': '30px'
-        });*/
+        $(window).on('load resize',windowSize);
 
         $container.css({
             'display': 'none'
@@ -289,7 +236,6 @@ $(document).ready(function() {
             '-webkit-transition': '200ms',
             'display': 'block',
             'font-size': '30px',
-            'left': '4%',
         });
 
         $bgdark.css({
@@ -385,5 +331,219 @@ $(document).ready(function() {
 
     $input.blur(function (e) {
         $(this).toggleClass('filled', !!$(this).val());
+    });
+
+
+    var animFalse = function() {
+        animation = false;
+    };
+    /*Открытие блока с полной информацией первой страницы*/
+    var active = function() {
+        if (animation) return;
+        var cellData = $(this).data('cell');
+        var $content = $('.box__content[data-content=' + cellData + ']');
+        animation = true;
+
+        $cell.addClass('cell-fade');
+        $(this).addClass('active');
+        $content.addClass('show-content');
+        $closeBtn2.addClass('box-close-active');
+
+        $('.box-close-active').css({
+            'display': 'block',
+            'transition': '2s'
+        });
+
+        $closeBtn.removeClass('show-close');
+        $iconTwitter.removeClass('icon-show');
+
+        setTimeout(animFalse, animDur);
+    };
+    /*Зыкрытие блока с полной информацией первой страницы*/
+    var close = function() {
+        animation = true;
+
+        $cell.removeClass('active cell-fade');
+        $content.removeClass('show-content');
+        $(this).removeClass('box-close-active');
+
+        $('.box__close').css({
+            'display': 'none'
+        });
+
+        $closeBtn.addClass('show-close');
+        $iconTwitter.addClass('icon-show');
+
+        setTimeout(animFalse, animDur);
+    };
+
+    <!-- CONTENT SLIDE TEACHERS-->
+    const persons = [
+        {
+            name: "Amber Gibs",
+            photo: "https://serving.photos.photobox.com/53464066dc132288128cb11531bb767f0082dcde75f3b845cd905d38c4223e95e0dc7bc9.jpg",
+            title: "Developer",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Andrey Grizodub",
+            photo: "https://sun9-21.userapi.com/c849232/v849232101/1d6f2f/nilWa9vgT7Y.jpg",
+            title: "Hip-Hop",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Sergey Mihutkin",
+            photo: "https://sun9-76.userapi.com/c850624/v850624965/18ab92/qUoaHK6IgCg.jpg",
+            title: "K-Pop",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Alice Jenkins",
+            photo: "https://serving.photos.photobox.com/52898788b03c8a0e32a8cb52b4d43d7525f119daa2629569dbda0a8d827192217fb64c22.jpg",
+            title: "QA Engineer",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Amber Gibs",
+            photo: "https://serving.photos.photobox.com/53464066dc132288128cb11531bb767f0082dcde75f3b845cd905d38c4223e95e0dc7bc9.jpg",
+            title: "Developer",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Andrey Grizodub",
+            photo: "https://sun9-21.userapi.com/c849232/v849232101/1d6f2f/nilWa9vgT7Y.jpg",
+            title: "Hip-Hop",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Sergey Mihutkin",
+            photo: "https://sun9-76.userapi.com/c850624/v850624965/18ab92/qUoaHK6IgCg.jpg",
+            title: "K-Pop",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        },
+        {
+            name: "Alice Jenkins",
+            photo: "https://serving.photos.photobox.com/52898788b03c8a0e32a8cb52b4d43d7525f119daa2629569dbda0a8d827192217fb64c22.jpg",
+            title: "QA Engineer",
+            bio:
+              "<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Amet architecto ea blanditiis quo labore esse magnam illum ut quibusdam. Corrupti ratione iure aliquam adipisci! Harum vitae laboriosam temporibus illo suscipit?</p><p>Saepe repudiandae rerum quam ut perferendis, ullam similique nemo quod, assumenda mollitia consectetur. Eveniet optio maxime perferendis odit possimus? Facilis architecto nesciunt doloribus consectetur culpa veritatis accusamus expedita quos voluptate!</p><p>Nisi provident minus possimus optio voluptate rem, perspiciatis, placeat, culpa aperiam quod temporibus.</p>",
+            social: {
+                facebook: "#",
+                twitter: "https://twitter.com/knyttneve",
+                linkedin: "#"
+            }
+        }
+    ];
+
+    const app = new Vue({
+        el: "#app",
+        data() {
+            return {
+                persons: persons,
+                selectedPersonIndex: null,
+                isSelected: false,
+                selectedPerson: null,
+                inlineStyles: null,
+                isReady: false,
+                isOk: false,
+                selectedPersonData: {
+                    name: null,
+                    title: null,
+                    photo: null,
+                    social: {
+                        facebook: null,
+                        twitter: null,
+                        linkedin: null
+                    }
+                }
+            };
+        },
+        methods: {
+            selectPerson(index, el) {
+                if (!this.isOk) {
+                    this.selectedPersonIndex = index;
+                    this.isSelected = true;
+                    el.target.parentElement.className == "person-details"
+                      ? (this.selectedPerson = el.target.parentElement.parentElement)
+                      : (this.selectedPerson = el.target.parentElement);
+
+                    this.selectedPerson.classList.add("person-selected");
+                    this.selectedPerson.setAttribute(
+                      "style",
+                      `width:${this.selectedPerson.offsetWidth}px;`
+                    );
+                    this.selectedPersonData = this.persons[index];
+                    window.setTimeout(() => {
+                        this.inlineStyles = `width:${this.selectedPerson
+                          .offsetWidth}px;height:${this.selectedPerson
+                          .offsetHeight}px;left:${this.selectedPerson.offsetLeft}px;top:${this
+                          .selectedPerson.offsetTop}px;position:fixed`;
+                        this.selectedPerson.setAttribute("style", this.inlineStyles);
+                    }, 400);
+                    window.setTimeout(() => {
+                        this.isReady = true;
+                        this.isOk = true;
+                    }, 420);
+                } else {
+                    this.reset();
+                }
+            },
+            reset() {
+                this.isReady = false;
+                window.setTimeout(() => {
+                    this.selectedPerson.classList.add("person-back");
+                }, 280);
+                window.setTimeout(() => {
+                    this.selectedPerson.setAttribute("style", "");
+                }, 340);
+                window.setTimeout(() => {
+                    this.isSelected = false;
+                    this.selectedPerson.classList.remove("person-back", "person-selected");
+                    this.isOk = false;
+                }, 400);
+            }
+        }
     });
 });
