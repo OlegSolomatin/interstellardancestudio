4f3fcce87198c85167864197fbfa059d1bd88387
OlegSolomatin <olegsolomatin2018@yandex.ru>
2020-08-25 15:45:03 +0300

    update 1. request api vk

--- js/social_zapros.js
+++ js/social_zapros.js
@@ -1,11 +1,15 @@
 $(document).ready(function () {
-		var baseUrL = 'https://api.vk.com/method/groups.getById?group_id=85050907&fields=members_count&v=5.80&access_token=1a90f1471a90f1471a90f147e41ae36bdb11a901a90f14745d39452ab0babaf258a286b';
-		fetch(baseUrL).then((response) => response.json()).then((res) => {
-			if (res.error == 200) {
-				console.log('lol');
-				$('#vkontakte_count').text(res.data.response.members_count);
-			}
-		}).catch((error) => {
-			console.log(error)
-		})
+	let script = document.createElement('SCRIPT');
+	script.src = "https://api.vk.com/method/groups.getById?group_id=85050907&fields=members_count&v=5.80&access_token=1a90f1471a90f1471a90f147e41ae36bdb11a901a90f14745d39452ab0babaf258a286b&callback=callbackFunc";
+	document.getElementsByTagName("head")[0].appendChild(script);
+	window.callbackFunc = function (result) {
+		document.getElementById('vkontakte_count').innerText = result.response[0].members_count;
+		//const count = JSON.parse(result.response[0].members_count);
+		//console.log(count);
+		//$('#vkontakte_count').text(result.response[0].members_count);
+		//const member = result.response[0].members_count;
+		//$('#vkontakte_count').innerHTML = member.toString();
+		//$('#vkontakte_count').text(member);
+		//console.log(member.toString());
+	}
 });
